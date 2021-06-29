f3f180cc5d8334c78ff1febd865f39fd68cdf7d0
rtmlsh <tyoma-tt@rambler.ru>
2021-06-29 18:45:40 +0300

    Получил укороченную ссылку с редиректом

--- click_counter.py
+++ click_counter.py
@@ -1,8 +1,8 @@
 import requests
 
-
-header = {'Authorization': 'Bearer 4b7bfd41c0bd5262c762e6b209dbd9b2b170fa1f'}
-url = 'https://api-ssl.bitly.com/v4/user'
-response = requests.get(url, headers=header)
+header = {'Authorization': '17c09e22ad155405159ca1977542fecf00231da7'}
+payload = {'long_url': 'https://yandex.ru/'}
+url = 'https://api-ssl.bitly.com/v4/bitlinks'
+response = requests.post(url, headers=header, json=payload)
 response.raise_for_status()
 print(response.text)
