de161125e50453b2f383154bf1dfaf782bd1a728
rtmlsh <tyoma-tt@rambler.ru>
2021-06-30 00:35:22 +0300

    Добавил функцию создания битссылок

--- click_counter.py
+++ click_counter.py
@@ -1,8 +1,15 @@
 import requests
 
-header = {'Authorization': '17c09e22ad155405159ca1977542fecf00231da7'}
-payload = {'long_url': 'https://yandex.ru/'}
+
+TOKEN = '17c09e22ad155405159ca1977542fecf00231da7'
 url = 'https://api-ssl.bitly.com/v4/bitlinks'
-response = requests.post(url, headers=header, json=payload)
-response.raise_for_status()
-print(response.text)
+link = input('Введите URL для сокращения: ')
+
+def shorten_link(url=url, token=TOKEN, link=link):
+    header = {'Authorization': f'{token}'}
+    payload = {'long_url': f'{link}'}
+    response = requests.post(f'{url}', headers=header, json=payload)
+    response.raise_for_status()
+    return (response.json()['link'])
+
+print('Битлинк', shorten_link())
