7aa85ef15648f63204631bc1d0e7abd7eaf0b945
rtmlsh <tyoma-tt@rambler.ru>
2021-07-03 13:32:48 +0300

    Добавил ифмейн

--- click_counter.py
+++ click_counter.py
@@ -2,18 +2,15 @@ import os
 import requests
 from dotenv import load_dotenv
 
-load_dotenv()
 
-TOKEN = os.getenv('TOKEN')
-link = input('Введите URL: ')
-
-def define_link(link=link, token = TOKEN):
+def define_link(link, token):
     url = 'https://api-ssl.bitly.com/v4/bitlinks/'f'{link}'
     header = {'Authorization': f'{token}'}
     response = requests.get(url, headers=header)
     check_clicks_count() if response else check_short_link()
 
-def shorten_link(token=TOKEN, link=link):
+
+def shorten_link(link, token):
     url = 'https://api-ssl.bitly.com/v4/bitlinks'
     header = {'Authorization': f'{token}'}
     payload = {'long_url': f'{link}'}
@@ -21,41 +18,41 @@ def shorten_link(token=TOKEN, link=link):
     response.raise_for_status()
     return (response.json()['link'])
 
-def check_short_link():
-    try:
-        bitlink = shorten_link()
-        print('Битссылка:', bitlink)
-    except requests.exceptions.HTTPError:
-        print('Вы ввели неверную ссылку 1')
 
-def count_clicks(link=link, token=TOKEN):
+def count_clicks(link, token):
     url = 'https://api-ssl.bitly.com/v4/bitlinks/'f'{link}''/clicks/summary'
     headers = {'Authorization': 'Bearer 'f'{token}'}
     payload = {
-        'unit': 'month',
+        'unit': 'day',
         'units': -1
     }
     response = requests.get(url, headers=headers, params=payload)
     response.raise_for_status()
     return response.json()['total_clicks']
 
+
+def check_short_link():
+    try:
+        bitlink = shorten_link(link=link, token=token)
+        print('Битссылка:', bitlink)
+    except requests.exceptions.HTTPError:
+        print('Вы ввели неверную ссылку')
+
+
 def check_clicks_count():
     try:
-        clicks_count = count_clicks()
+        clicks_count = count_clicks(link=link, token=token)
         print('Кликов:', clicks_count)
     except requests.exceptions.HTTPError:
-        print('Вы ввели неверную ссылку 2')
+        print('Вы ввели неверную ссылку')
 
 
+def main():
+    define_link(link, token)
 
 
-
-
-# https://bit.ly/2GE4ZC3
-# https://yandex.ru/
-
-define_link()
-
-
-
-
+if __name__ == "__main__":
+    load_dotenv()
+    token = os.getenv('TOKEN')
+    link = input('Введите URL: ')
+    main()
