624ff239497bd85aefde807200f67297af8961b3
rtmlsh <56894987+rtmlsh@users.noreply.github.com>
2021-07-14 13:31:45 +0300

    Отформатировал код

--- main.py
+++ main.py
@@ -6,10 +6,14 @@ from dotenv import load_dotenv
 
 
 def create_parser():
-    parser = argparse.ArgumentParser(description='Скрипт позволяет получить битлинк из\
-    ссылки и посчитать количество кликов по ней')
-    parser.add_argument('link', help='Передайте битлинк или ссылку в\
-    качестве аргумента при запуске скрипта')
+    parser = argparse.ArgumentParser(
+        description='Скрипт позволяет получить битлинк из '
+                    'ссылки и посчитать количество кликов по ней'
+    )
+    parser.add_argument(
+        'link', help='Передайте битлинк или ссылку в качестве '
+                     'аргумента при запуске скрипта'
+    )
     return parser
 
 
